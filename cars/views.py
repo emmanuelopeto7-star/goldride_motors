d3a4e764c982609365768acece02f17688a7bcdd
emmanuelopeto7-star <emmanuelopeto7@gmail.com>
2026-07-21 21:32:49 +0300

    creating cars api

--- cars/views.py
+++ cars/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Car
+from .serializers import CarSerializer
 
-# Create your views here.
+class carListVeiw(generics.ListAPIView):
+    queryset = Car.objects.all()
+    serializer_class = CarSerializer
