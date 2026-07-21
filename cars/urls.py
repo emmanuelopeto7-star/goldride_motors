2f4a93985c8d887126182f067d6ea6bc15915507
emmanuelopeto7-star <emmanuelopeto7@gmail.com>
2026-07-21 21:44:57 +0300

    adding car detail endpoint

--- cars/urls.py
+++ cars/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import carListVeiw
+from .views import carListVeiw, carDetailView
 
 urlpatterns = [
      path("", carListVeiw.as_view()),
+    path("<int:pk>/", carDetailView.as_view()),
 ]
