c1ea50701c0fccd7d72be0f97815feacf43ae5e0
emmanuelopeto7-star <emmanuelopeto7@gmail.com>
2026-07-21 21:44:57 +0300

    adding car detail endpoint

--- cars/views.py
+++ cars/views.py
@@ -5,3 +5,6 @@ from .serializers import CarSerializer
 class carListVeiw(generics.ListAPIView):
     queryset = Car.objects.all()
     serializer_class = CarSerializer
+class carDetailView(generics.RetrieveAPIView):
+    queryset = Car.objects.all()
+    serializer_class = CarSerializer
