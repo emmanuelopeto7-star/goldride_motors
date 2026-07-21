4314c049fd8fad72104e01ee8ff84dbe8e75d5bc
emmanuelopeto7-star <emmanuelopeto7@gmail.com>
2026-07-21 21:08:03 +0300

    creating car app

---
+++ cars/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+class Car(models.Model):
+    state_choices = [
+        ('new', 'New'),
+        ('used', 'Used'),
+        ("available", "Available"),
+        ("reserved", "Reserved"),
+        ("sold", "Sold"),
+    ]
+    make=models.CharField(max_length=50)
+    model=models.CharField(max_length=50)
+    year=models.PositiveIntegerField()
+    price=models.DecimalField(max_digits=12, decimal_places=2)
+    state=models.CharField(max_length=10, choices=state_choices, default='available')
+    description=models.TextField()
+
+    def __str__(self):
+        return f"{self.make} {self.model} ({self.year}) - {self.state} - ${self.price} - {self.description}"
