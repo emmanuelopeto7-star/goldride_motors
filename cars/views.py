834cb12a79c58d48c01f95017e3672733d101b3c
emmanuelopeto7-star <emmanuelopeto7@gmail.com>
2026-07-22 11:47:41 +0300

    addition of filtering and searching

--- cars/views.py
+++ cars/views.py
@@ -1,10 +1,14 @@
-from rest_framework import generics
+from rest_framework import generics,filters
 from .models import Car
 from .serializers import CarSerializer
+from django_filters.rest_framework import DjangoFilterBackend
 
 class carListVeiw(generics.ListAPIView):
     queryset = Car.objects.all()
     serializer_class = CarSerializer
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_fields = ['make', 'model', 'year', 'state']
+    ordering_fields = ['price', 'year']
 class carDetailView(generics.RetrieveAPIView):
     queryset = Car.objects.all()
     serializer_class = CarSerializer
