e2b3767f2b13e83932a880fbb342708b43611481
emmanuelopeto7-star <emmanuelopeto7@gmail.com>
2026-07-22 11:00:15 +0300

    adding throttle inquiries endpoint

--- inquiries/views.py
+++ inquiries/views.py
@@ -1,7 +1,10 @@
 from rest_framework import generics
 from .models import Inquiry
 from .serializers import InquirySerializer
+from rest_framework.throttling import ScopedRateThrottle
 
 class InquiryCreateView(generics.CreateAPIView):
     queryset = Inquiry.objects.all()
     serializer_class = InquirySerializer
+    throttle_classes = [ScopedRateThrottle]
+    throttle_scope = 'inquiries'
