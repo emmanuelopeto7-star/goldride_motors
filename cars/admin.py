13968dae23ed709856a8329a919d8ffc680202d9
emmanuelopeto7-star <emmanuelopeto7@gmail.com>
2026-07-24 20:07:57 +0300

    add car image gallery

--- cars/admin.py
+++ cars/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
-from .models import Car
+from .models import Car, CarImage
 
+class CarImageInline(admin.TabularInline):
+    model = CarImage
+    extra = 3
 class CarAdmin(admin.ModelAdmin):
     list_display = ["make", "model", "year", "price", "condition", "availability"]
     list_filter = ["condition", "availability", "make"]
     search_fields = ["make", "model", "description"]
     ordering = ["year"]
-admin.site.register(Car)
+    inlines = [CarImageInline]
+admin.site.register(Car, CarAdmin)
