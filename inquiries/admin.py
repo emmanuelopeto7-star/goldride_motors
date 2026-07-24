c05ac83aff4f0f491893d05cf048bb88aee04739
emmanuelopeto7-star <emmanuelopeto7@gmail.com>
2026-07-24 19:35:18 +0300

    polishing car and inruiry

--- inquiries/admin.py
+++ inquiries/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Inquiry
-
+class InquiryAdmin(admin.ModelAdmin):
+    list_display = ["name", "phone", "car", "created_at"]
+    list_filter = ["created_at"]
+    search_fields = ["name", "phone"]
+    ordering = ["-created_at"]
+    readonly_fields = ["created_at"]
 admin.site.register(Inquiry)
 
