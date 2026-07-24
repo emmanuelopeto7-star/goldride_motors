c11943bb44c93dfdf50c1ded2001ef8ab4616a8b
emmanuelopeto7-star <emmanuelopeto7@gmail.com>
2026-07-24 19:35:18 +0300

    polishing car and inruiry

--- cars/admin.py
+++ cars/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import Car
 
+class CarAdmin(admin.ModelAdmin):
+    list_display = ["make", "model", "year", "price", "state"]
+    list_filter = ["state", "make"]
+    search_fields = ["make", "model", "description"]
+    ordering = ["year"]
 admin.site.register(Car)
