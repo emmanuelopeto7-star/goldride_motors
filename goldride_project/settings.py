7dd46a035fdfe1a66c396380eb2d1debf07d35f9
emmanuelopeto7-star <emmanuelopeto7@gmail.com>
2026-07-28 10:35:09 +0300

    email alert on new inquiry

--- goldride_project/settings.py
+++ goldride_project/settings.py
@@ -116,6 +116,8 @@ REST_FRAMEWORK = {
         "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 12,
 }
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+DEFAULT_FROM_EMAIL = 'noreply@goldridemotors.co.ke'
 
 
 # Internationalization
--- inquiries/views.py
+++ inquiries/views.py
@@ -2,9 +2,20 @@ from rest_framework import generics
 from .models import Inquiry
 from .serializers import InquirySerializer
 from rest_framework.throttling import ScopedRateThrottle
+from django.core.mail import send_mail
 
 class InquiryCreateView(generics.CreateAPIView):
     queryset = Inquiry.objects.all()
     serializer_class = InquirySerializer
     throttle_classes = [ScopedRateThrottle]
     throttle_scope = 'inquiries'
+
+    def perform_create(self, serializer):
+        inquiry = serializer.save()
+        send_mail(
+            subject=f"New inquiry: {inquiry.car}",
+            message=f"{inquiry.name} ({inquiry.phone}) is interested in {inquiry.car}.\n\n{inquiry.message}",
+            from_email=None,
+            recipient_list=["sales@goldridemotors.co.ke"],
+            fail_silently=True,
+        )
