20c5993cdd9856001035feb1c83cae606fc574f0
emmanuelopeto7-star <emmanuelopeto7@gmail.com>
2026-07-30 09:58:09 +0300

    register payment admin

--- payments/admin.py
+++ payments/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Payment
+
+
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ["reference", "order", "amount", "method", "status", "created_at"]
+    list_filter = ["status", "method"]
+    search_fields = ["reference", "provider_ref", "note"]
+    readonly_fields = ["reference", "provider_ref", "created_at", "updated_at"]
+
+
+admin.site.register(Payment, PaymentAdmin)
