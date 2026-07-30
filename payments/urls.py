618f28f1d7319149d19e2fe79070e5d982859e79
emmanuelopeto7-star <emmanuelopeto7@gmail.com>
2026-07-30 09:44:17 +0300

    creating paystack webhook

--- payments/urls.py
+++ payments/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from.views import InitiatePaymentView
+from .views import InitiatePaymentView, PaystackWebhookView
+
 
 urlpatterns = [
     path("initiate/", InitiatePaymentView.as_view()),
+    path("webhook/", PaystackWebhookView.as_view()),
 ]
