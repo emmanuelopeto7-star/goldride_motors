bb4c75779054a0353f789772b2af534515fe8e59
emmanuelopeto7-star <emmanuelopeto7@gmail.com>
2026-08-11 10:35:40 +0300

    use fast password hasher in tests

--- backend/goldride_project/settings.py
+++ backend/goldride_project/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import sys
 from pathlib import Path
 from decouple import config
 import os
